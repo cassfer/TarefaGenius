5897f3644a7213d9e50491611f7bf6634c6a1677
cassfer <cassiohafernandes@gmail.com>
2023-06-09 00:50:30 -0300

    09-06-2023 - Adicionado teste unitario do /health

---
+++ src/test/java/br/com/fernandes/controllers/TarefaGeniusControllerTest.java
@@ -0,0 +1,32 @@
+package br.com.fernandes.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class TarefaGeniusControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void testCheckHealth() throws Exception{
+		MvcResult result = mockMvc.perform(get("/health")).andReturn();
+		
+		int status = result.getResponse().getStatus();
+		String content = result.getResponse().getContentAsString();
+		
+		assertEquals(200, status);
+		assertEquals("Aplicação está saudável", content);
+		
+	}
+	
+}
