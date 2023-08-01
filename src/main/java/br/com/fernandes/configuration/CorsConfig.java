07ab7c190c77defb8522205e96ecadd831a85391
cassfer <cassiohafernandes@gmail.com>
2023-08-01 17:25:16 -0300

    01-08-2023 - Atualizacao com cons de localhost e no servidor onrender

---
+++ src/main/java/br/com/fernandes/configuration/CorsConfig.java
@@ -0,0 +1,18 @@
+package br.com.fernandes.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:4200", "https://tarefa-genius.onrender.com")
+			.allowedHeaders("*")
+			.allowedMethods("GET", "POST", "DELETE", "PUT")
+			.allowCredentials(true);
+	}
+	
+}
